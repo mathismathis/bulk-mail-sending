dd28da324b1ff1c4c70cb0dae2133474b526de64
T.Mathis <51128789+mathismathis@users.noreply.github.com>
2020-04-10 12:34:04 +0530

    Add files via upload
    how to send bulk gmails  by using  smtplib

---
+++ script.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import smtplib
+
+excel= pd.read_excel("gmail.xlsx")
+gmails = excel['gmails'].values
+print(gmails)
+
+username="rtech654321@gmail.com"
+password="ytstamil001"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(username,password)
+print("login suscess")
+
+msg = "hi Welcome"
+subject = "Testing"
+body = "Subject: {}\n\n{}".format(subject,msg)
+
+for mail in gmails:
+    server.sendmail(username, mail, body)
+server.quit()
